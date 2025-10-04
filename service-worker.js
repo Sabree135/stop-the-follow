7504a62e00f37a31e7cb78282f8a9f9cffe4d1a3
Sabree135 <56363231+Sabree135@users.noreply.github.com>
2025-10-04 16:51:57 -0400

    Update and rename Disregard.txt to service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'stop-the-follow-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/resources.html',
+  '/script.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
